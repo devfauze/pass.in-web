8db7d60765b4332713a75db95d487e4904389ef0
Fauze Cavalari <fauze.cavalari@gmail.com>
2024-04-08 10:56:24 -0300

    FEAT: web application

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import passInIcon from '../assets/pass-in-icon.svg'
+import { NavLink } from './nav-link'
+
+export function Header(){
+    return(
+        <div className='flex items-center gap-5 py-2'>
+            <img src={passInIcon} alt="Pass In Icon" />
+
+            <nav className='flex items-center gap-5'>
+                <NavLink href='/eventos'>Eventos</NavLink>
+                <NavLink href='/participantes'>Participantes</NavLink>
+            </nav>
+        </div>
+    )
+}
